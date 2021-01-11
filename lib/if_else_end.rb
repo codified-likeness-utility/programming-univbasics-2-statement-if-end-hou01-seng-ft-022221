1696d02468c5b701a35d9b1bc322ffd598e1aa90
Ian Davies <mr.daviesian@gmail.com>
2021-01-11 21:44:57 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,10 +5,10 @@
 current_time = Time.now
 
 # convert current time to Integer
-current_time = current.time.to_i
+current_time = current_time.to_i
 
 # If the current second is an even number output "Even!"
-  if TRUE
+  if current_time % 2 == 0
     puts "Even!"
   # else output "Odd!"
   else
