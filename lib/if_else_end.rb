bc63dfd81df697336320243ba8e3acea3c9ca26e
Ian Davies <mr.daviesian@gmail.com>
2021-01-11 21:45:42 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,17 @@
 # Write your solution here
+# Write if...else...end statement
+
+# define current time local_variable
+current_time = Time.now
+
+# convert current time to Integer
+current_time = current_time.to_i
+
+# If the current second is an even number output "Even!"
+  if current_time % 2 == 0
+    puts "Even!"
+  # else output "Odd!"
+  else
+    puts "Odd!"
+  # end
+  end
